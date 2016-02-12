7c05fedb927634fa8336a9becdb4fc22b213a281
Daniel Beck <daniel.e.beck@gmail.com>
2016-02-12 12:52:17 +0000

    script to train on wmt15 data

---
+++ src/wqe15.py
@@ -0,0 +1,39 @@
+import wqe
+import numpy as np
+import sys
+import os
+from imitation.state import State
+
+TRAIN_DIR = sys.argv[1]
+TEST_DIR = sys.argv[2]
+
+def load_data(_dir, mode):
+    with open(os.path.join(_dir, mode + '.target')) as f:
+        mt = [line.split() for line in f.readlines()]
+    with open(os.path.join(_dir, mode + '.tags')) as f:
+        tags = [line.split() for line in f.readlines()]
+    instances = [wqe.WQEInstance(x, y) for x, y in zip(mt, tags)]
+    return instances
+
+train_instances = load_data(TRAIN_DIR, 'train')#[:000]
+test_instances = load_data(TEST_DIR, 'test')[:10]
+
+#print [(inst.input.tokens, inst.output.tags) for inst in train_instances[:5]]
+
+model = wqe.WQE()
+
+# set the params
+params = wqe.WQE.params()
+# Setting this to one means on iteration, i.e. exact imitation. The learning rate becomes irrelevant then
+params.iterations = 1
+params.learningParam = 0.3
+params.samplesPerAction = 1
+
+    
+model.train(train_instances, "temp", params)
+# TODO: This is a hack. Probably the state initialization should happen in the beginning of predict
+state = State()
+print model.predict(test_instances[0], state).tags
+print test_instances[0].output.tags
+#state = State()
+#print model.predict(test_instances[0], state).tags
